cd21479ead5e79ec6bc28ca6069ee7a703da8c51
Asmae Mouradi <amouradi2002@gmail.com>
2023-07-09 19:32:03 +0100

    adding the columns by grid template

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,24 +21,36 @@ const Contact = () => {
       <div className="relative w-full h-1/3">
         <div className="absolute inset-0 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${contactBackground})` }}></div>
       </div>
-      <div className="flex flex-col items-center justify-center py-8">
-        <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
-        <div className="flex items-center mb-4">
-          <RiPhoneLine className="w-6 h-6 mr-2" />
+      <div className="gap-20 grid grid-cols-3"  >
+      <div>
+        <div>
+          <RiPhoneLine w-6 h-6 mr-2 />
           <p>Phone Number: +212 668-543004</p>
         </div>
-        <div className="flex items-center mb-4">
+      </div>
+      <div>
+      <div>
           <RiMailLine className="w-6 h-6 mr-2" />
           <p>Email Address: easymaroc07@gmail.com</p>
         </div>
-        <div className="flex">
+      </div>
+      <div>
+      <div>
           <a href="https://www.facebook.com/easylarache" target="_blank" rel="noopener noreferrer" className="mr-2">
             <RiFacebookLine className="w-6 h-6" />
           </a>
+          <span>
           <a href="https://www.instagram.com/easy.maroc/?igshid=MzRlODBiNWFlZA%3D%3D" target="_blank" rel="noopener noreferrer">
             <RiInstagramLine className="w-6 h-6" />
           </a>
+          </span>
         </div>
+      </div>
+      </div>
+      <div className="flex flex-col items-center justify-center py-8">
+        
+        
+        
       </div>
     </div>
   );
