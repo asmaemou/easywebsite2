3b554f3b6325cd527d4f10a985fa0a35856d8b69
Asmae Mouradi <amouradi2002@gmail.com>
2023-07-11 23:21:17 +0100

    trying to add the footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import Contact from './components/Contact';
 import About from './components/About';
 import Services from './components/Services';
+import Footer from './components/Footer';
 
 // import { useTranslation, I18nextProvider } from 'react-i18next';
 // import i18n from './i18n'; // Import your i18n configuration
@@ -18,7 +19,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
+        <Footer/>
       </Routes> 
+      <Footer/>
     </>
   );
 }
