0db4fd17f93a378a59e2ea7c029c6c4ac35f83bb
Asmae Mouradi <amouradi2002@gmail.com>
2023-07-13 23:50:50 +0100

    footer+service page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,14 @@ import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white mt-20">
+    <footer className="bg-[#eff3ff] text-white mt-20">
       <div
         className="flex flex-row justify-between w-10/12 mx-auto mt-5 items-center
       text-center pt-2 text-gray-400 text-sm"
       >
         <span>© 2020 Appy. All rights reserved.</span>
         <span>Terms · Privacy Policy</span>
-        <div className="flex flex-row justify-between ">
+        <div className="flex flex-row justify-between space-x-4">
         <SocialIcon url="https://web.facebook.com/easylarache/videos/3293695087585784" bgColor='#079e7c' />
         <SocialIcon url="https://www.instagram.com/easy.maroc/?igshid=MzRlODBiNWFlZA%3D%3D" bgColor='#079e7c' />
         </div>
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,29 +1,26 @@
 
 const Services = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold mb-4">Services</h1>
-      <div className="grid grid-cols-3 gap-8">
-        <div className="bg-gray-100 p-4">
-          <h2 className="text-lg font-bold mb-2"> Easy partner sell more</h2>
-          <textarea
-            className="w-full h-32 p-2 border border-gray-300 rounded"
-            placeholder="Write your paragraph here"
-          ></textarea>
+    <div className="flex flex-col items-center w-10/12 mx-auto mt-20 h-screen">
+      <h1 className="text-5xl font-bold mb-4">Our Services</h1>
+      <div>
+        <p className="font-bold mb-2 text-center mt-7 mb-17 text-2xl">Two Types of Clients</p>
+        <div className="flex gap-20 mt-8 w-10/12 mx-auto">
+        <div className="bg-[#fec23f] p-6 rounded-lg hover:-translate-y-5 hover:-translate-x-2 duration-300">
+
+          <h1 className="font-bold mb-2 text-center">Easy partner sell more:</h1>
+          <p className="text-lg">
+          We propose to our users to subscribe in the EASY app which enables you to face price rising through the discounts received when you make any purchase at one of the EASY partner shops you find in the app. The app makes you up to date with the special offers proposed by the partners that appear below the app
+            </p>
+          </div>
+
+          <div className="bg-[#fec23f] p-6 rounded-lg hover:-translate-y-5 hover:translate-x-2 duration-300">
+          <h1 className="font-bold mb-2 text-center">Easy client save more:</h1>
+        <p className="text-lg">
+        We propose to our users to subscribe in the EASY app which enables you to face price rising through the discounts received when you make any purchase at one of the EASY partner shops you find in the app. The app makes you up to date with the special offers proposed by the partners that appear below the app
+        </p>
         </div>
-        <div className="bg-gray-100 p-4">
-          <h2 className="text-lg font-bold mb-2">  Easy client save more</h2>
-          <textarea
-            className="w-full h-32 p-2 border border-gray-300 rounded"
-            placeholder="Write your paragraph here"
-          ></textarea>
-        </div>
-        <div className="bg-gray-100 p-4">
-          <h2 className="text-lg font-bold mb-2">Block 3</h2>
-          <textarea
-            className="w-full h-32 p-2 border border-gray-300 rounded"
-            placeholder="Write your paragraph here"
-          ></textarea>
+
         </div>
       </div>
     </div>
