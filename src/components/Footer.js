df2fd3fb88c8da1f7470c00e55d43f8fcc290bc6
Asmae Mouradi <amouradi2002@gmail.com>
2023-07-14 17:39:04 +0100

    make the footer appropriate

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,13 @@ const Footer = () => {
     <footer className="bg-[#eff3ff] text-white mt-20">
       <div
         className="flex flex-row justify-between w-10/12 mx-auto mt-5 items-center
-      text-center pt-2 text-gray-400 text-sm"
+      text-center pt-8 text-gray-400 text-sm"
       >
         <span>© 2020 Appy. All rights reserved.</span>
         <span>Terms · Privacy Policy</span>
         <div className="flex flex-row justify-between space-x-4">
-        <SocialIcon url="https://web.facebook.com/easylarache/videos/3293695087585784" bgColor='#079e7c' />
-        <SocialIcon url="https://www.instagram.com/easy.maroc/?igshid=MzRlODBiNWFlZA%3D%3D" bgColor='#079e7c' />
+        <SocialIcon url="https://web.facebook.com/easylarache/videos/3293695087585784" bgColor='#079e7c' size="15" style={{ marginTop: "-3px"}} />
+        <SocialIcon url="https://www.instagram.com/easy.maroc/?igshid=MzRlODBiNWFlZA%3D%3D" bgColor='#079e7c' size="15" style={{marginTop:"-3px"}} />
         </div>
         
       </div>
