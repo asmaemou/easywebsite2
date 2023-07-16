9822f9842590bd2b5a7bff13a6ad152d5cafc7e4
Asmae Mouradi <amouradi2002@gmail.com>
2023-07-16 23:48:54 +0100

    make the footer look better by creating footer.css +footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { SocialIcon } from 'react-social-icons';
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#eff3ff] text-white mt-20">
+    <footer className="footer bg-[#eff3ff] text-white mt-40">
       <div
-        className="flex flex-row justify-between w-10/12 mx-auto mt-5 items-center
-      text-center pt-8 text-gray-400 text-sm"
+        className="footer-container flex flex-row justify-center items-center w-10/12 mx-auto mt-5 text-center text-gray-400 text-sm"
       >
-        <span>© 2020 Appy. All rights reserved.</span>
-        <span>Terms · Privacy Policy</span>
-        <div className="flex flex-row justify-between space-x-4">
-        <SocialIcon url="https://web.facebook.com/easylarache/videos/3293695087585784" bgColor='#079e7c' size="15" style={{ marginTop: "-3px"}} />
-        <SocialIcon url="https://www.instagram.com/easy.maroc/?igshid=MzRlODBiNWFlZA%3D%3D" bgColor='#079e7c' size="15" style={{marginTop:"-3px"}} />
+        <span className="mr-4">© 2020 Appy. All rights reserved.</span>
+        <span className="mr-4">Terms · Privacy Policy</span>
+        <div className="flex flex-row justify-center space-x-4">
+          <SocialIcon url="https://web.facebook.com/easylarache/videos/3293695087585784" bgColor='#079e7c' size="15" style={{ width: "35px", height: "35px", marginLeft: "10px" }} />
+          <SocialIcon url="https://www.instagram.com/easy.maroc/?igshid=MzRlODBiNWFlZA%3D%3D" bgColor='#079e7c' size="15" style={{ width: "35px", height: "35px", marginLeft: "10px" }} />
+          <SocialIcon url="https://wa.me/message/H2BEQDJRQ63NM1" network="whatsapp" bgColor='#079e7c' size="15" style={{ width: "35px", height: "35px", marginLeft: "10px" }} />
         </div>
-        
       </div>
     </footer>
   );
 };
+
 export default Footer;
