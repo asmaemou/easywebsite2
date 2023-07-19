798e309267a4877dc5f4393e0499ca16883e56cc
Asmae Mouradi <amouradi2002@gmail.com>
2023-07-19 19:15:46 +0100

    solve the problems to deploy

--- src/translation.js
+++ src/translation.js
@@ -3,25 +3,25 @@ import { initReactI18next } from "react-i18next";
 import i18nextApi from "i18next-http-backend";
 // "Inline" English and Arabic translations.
 // We can localize to any language and any number of languages.
-const resources = {
-  en: {
-    translation: {
-      Home: "Home",
-      About: "About",
-      Services: "Services",
-      Contact: "Contact",
+// const resources = {
+//   en: {
+//     translation: {
+//       Home: "Home",
+//       About: "About",
+//       Services: "Services",
+//       Contact: "Contact",
       
-    },
-  },
-  ar: {
-    translation: {
-      Home: "بيت",
-      About: "عن",
-      Services:"خدماتنا",
-      Contact: "اتصل بنا",
-    },
-  },
-};
+//     },
+//   },
+//   ar: {
+//     translation: {
+//       Home: "بيت",
+//       About: "عن",
+//       Services:"خدماتنا",
+//       Contact: "اتصل بنا",
+//     },
+//   },
+// };
 i18next
   .use(initReactI18next)
   .use(i18nextApi)
